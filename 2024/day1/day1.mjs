740caad0b0b03964f74ab2432ce265ac8d78377f
Luis <84397555+LuisFadini@users.noreply.github.com>
2024-12-01 03:24:31 -0300

    Added day 1 JS based solution

--- 2024/day1/day1.mjs
+++ 2024/day1/day1.mjs
@@ -1,8 +1,8 @@
 import fs from "node:fs";
 
-const sample1 = fs.readFileSync("sample1.txt", { encoding: "utf-8" });
+const input = fs.readFileSync("input.txt", { encoding: "utf-8" });
 
-console.log(part1(sample1));
+console.log(part2(input));
 
 /**
  *
@@ -29,6 +29,45 @@ function part1(fileContent) {
     const rightDist = rightDists[i];
 
     distSum += Math.abs(leftDist - rightDist);
+  }
+
+  return distSum;
+}
+
+/**
+ *
+ * @param {String} fileContent
+ */
+function part2(fileContent) {
+  const lines = fileContent.trim().split("\r\n");
+  const leftDists = [];
+  const rightDists = [];
+
+  lines.map((l) => {
+    const [left, right] = l.split("   ");
+    leftDists.push(left);
+    rightDists.push(right);
+  });
+
+  let lookup = new Map();
+
+  let distSum = 0;
+
+  for (let i = 0; i < leftDists.length; i++) {
+    const leftDist = leftDists[i];
+    if (lookup.has(leftDist)) {
+      distSum += lookup.get(leftDist);
+      continue;
+    }
+
+    if (!rightDists.includes(leftDist)) {
+      continue;
+    }
+
+    let similarityScore =
+      rightDists.filter((f) => f === leftDist).length * leftDist;
+    lookup.set(leftDist, similarityScore);
+    distSum += similarityScore;
   }
 
   return distSum;
