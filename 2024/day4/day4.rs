c489772be31213a046b358544d239e94fc03da26
Luis <84397555+LuisFadini@users.noreply.github.com>
2024-12-04 17:30:49 -0300

    Added day 4 part 2 Rust based solution

--- 2024/day4/day4.rs
+++ 2024/day4/day4.rs
@@ -4,7 +4,6 @@ fn part1(input_data: String) -> i32 {
     let input_data_lines: Vec<&str> = input_data.lines().collect();
     let mut valid_sequences = 0;
 
-    // Iterate through each line and its characters
     for (i, line) in input_data_lines.iter().enumerate() {
         for (j, _) in line.chars().enumerate() {
             valid_sequences += valid_xmas_sequences(input_data_lines.clone(), i, j);
@@ -60,6 +59,43 @@ fn valid_xmas_sequences(lines: Vec<&str>, i: usize, j: usize) -> i32 {
     valid_sequences
 }
 
+fn part2(input_data: String) -> i32 {
+    let input_data_lines: Vec<&str> = input_data.lines().collect();
+    let mut valid_sequences = 0;
+
+    for i in 1..input_data_lines.len() - 1 {
+        let line = input_data_lines[i];
+        for j in 1..line.len() - 1 {
+            let cur_char = get_char(line, j);
+
+            if cur_char == 'A' {
+                if get_neighbors(&input_data_lines, i, j) == ['M', 'M', 'S', 'S']
+                    || get_neighbors(&input_data_lines, i, j) == ['S', 'S', 'M', 'M']
+                    || get_neighbors(&input_data_lines, i, j) == ['M', 'S', 'M', 'S']
+                    || get_neighbors(&input_data_lines, i, j) == ['S', 'M', 'S', 'M']
+                {
+                    valid_sequences += 1
+                }
+            }
+        }
+    }
+
+    return valid_sequences;
+}
+
+fn get_char(line: &str, i: usize) -> char {
+    return line.chars().nth(i).unwrap();
+}
+
+fn get_neighbors(lines: &[&str], i: usize, j: usize) -> [char; 4] {
+    [
+        get_char(lines[i - 1], j - 1), // Top-left
+        get_char(lines[i - 1], j + 1), // Top-right
+        get_char(lines[i + 1], j - 1), // Bottom-left
+        get_char(lines[i + 1], j + 1), // Bottom-right
+    ]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -69,11 +105,15 @@ mod tests {
         let file_content = fs::read_to_string("./sample1.txt").unwrap();
         assert_eq!(part1(file_content), 18);
     }
+
+    #[test]
+    fn test2() {
+        let file_content = fs::read_to_string("./sample2.txt").unwrap();
+        assert_eq!(part2(file_content), 9);
+    }
 }
 
 fn main() {
-    // let file_content = fs::read_to_string("./sample1.txt").unwrap();
-    // println!("{}", part1(file_content));
     let input_path = env::args().nth(1);
     if input_path.is_none() {
         println!("Input path should be specified!");
@@ -82,6 +122,6 @@ fn main() {
 
     println!(
         "Output: {}",
-        part1(fs::read_to_string(input_path.unwrap()).unwrap())
+        part2(fs::read_to_string(input_path.unwrap()).unwrap())
     );
 }
