66b9226fb425514c5178d157e8b4324866928290
Luis <84397555+LuisFadini@users.noreply.github.com>
2024-12-08 13:03:01 -0300

    Added day 8 part 2 Rust based solution

--- 2024/day8/day8.rs
+++ 2024/day8/day8.rs
@@ -25,7 +25,13 @@ fn point_within(point: (i32, i32), width: i32, height: i32) -> bool {
     return point.0 >= 0 && point.1 >= 0 && point.0 <= width - 1 && point.1 <= height - 1;
 }
 
-fn part1(input_data: String) -> i32 {
+fn parse_input(
+    input_data: String,
+) -> (
+    HashMap<char, Vec<(i32, i32)>>,
+    i32,
+    i32,
+) {
     let lines = input_data
         .lines()
         .filter(|l| !l.is_empty())
@@ -42,11 +48,17 @@ fn part1(input_data: String) -> i32 {
             if char == '.' {
                 continue;
             }
-            let point = (x as i32, y as i32);
+            let point: (i32, i32) = (x as i32, y as i32);
             map.entry(char).or_default().push(point);
             points.insert(point);
         }
     }
+
+    return (map, width, height);
+}
+
+fn part1(input_data: String) -> i32 {
+    let (map, width, height) = parse_input(input_data);
 
     let mut antinodes = HashSet::new();
 
@@ -59,12 +71,10 @@ fn part1(input_data: String) -> i32 {
                 let antinode_1 = sub_point(min, diff);
                 let antinode_2 = add_point(max, diff);
 
-                if point_within(antinode_1, width, height)
-                {
+                if point_within(antinode_1, width, height) {
                     antinodes.insert(antinode_1);
                 }
-                if point_within(antinode_2, width, height)
-                {
+                if point_within(antinode_2, width, height) {
                     antinodes.insert(antinode_2);
                 }
             }
@@ -75,7 +85,32 @@ fn part1(input_data: String) -> i32 {
 }
 
 fn part2(input_data: String) -> i32 {
-    0
+    let (map, width, height) = parse_input(input_data);
+
+    let mut antinodes = HashSet::new();
+
+    for value in map.values() {
+        for &point_a in value.iter() {
+            for &point_b in value.iter().filter(|&&b| b != point_a) {
+                let diff = get_point_diff(point_a, point_b);
+                let (min, max) = (point_a.min(point_b), point_a.max(point_b));
+
+                let mut current_point = min;
+                while point_within(current_point, width, height) {
+                    antinodes.insert(current_point);
+                    current_point = sub_point(current_point, diff);
+                }
+
+                let mut current_point = max;
+                while point_within(current_point, width, height) {
+                    antinodes.insert(current_point);
+                    current_point = add_point(current_point, diff);
+                }
+            }
+        }
+    }
+
+    antinodes.len() as i32
 }
 
 #[cfg(test)]
@@ -104,6 +139,6 @@ fn main() {
 
     println!(
         "Output: {}",
-        part1(fs::read_to_string(input_path.unwrap()).unwrap())
+        part2(fs::read_to_string(input_path.unwrap()).unwrap())
     );
 }
