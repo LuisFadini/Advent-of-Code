e10709e48c35b9c65b88b42979c5c8488859dc71
Luis <84397555+LuisFadini@users.noreply.github.com>
2024-12-09 23:00:41 -0300

    Added day 9 part 2 Rust based solution

--- 2024/day9/day9.rs
+++ 2024/day9/day9.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process::exit};
+use std::{env, fs, ops::Range, process::exit};
 
 fn part1(input_data: String) -> u64 {
     let mut disk_content = Vec::new();
@@ -39,8 +39,43 @@ fn part1(input_data: String) -> u64 {
         .sum()
 }
 
-fn part2(input_data: String) -> i32 {
-    return 0;
+fn part2(input_data: String) -> u64 {
+    let mut files: Vec<(Range<usize>, u64)> = Vec::new();
+    let mut free_blocks: Vec<Range<usize>> = Vec::new();
+
+    let mut curr_i = 0;
+
+    for (index, size_char) in input_data.chars().enumerate() {
+        let space_size = size_char.to_digit(10).unwrap();
+        let space_range = curr_i as usize..curr_i as usize + space_size as usize;
+        curr_i = space_range.end;
+
+        if index % 2 == 0 {
+            files.push((space_range, index as u64 / 2));
+        } else {
+            free_blocks.push(space_range);
+        }
+    }
+
+    for file in files.iter_mut().rev() {
+        if let Some((i, free)) = free_blocks
+            .iter_mut()
+            .enumerate()
+            .find(|(_, f)| f.end <= file.0.start && f.len() >= file.0.len())
+        {
+            let free_start = free.start;
+            *free = free_start + file.0.len()..free.end;
+            *file = (free_start..free_start + file.0.len(), file.1);
+            if free.len() == 0 {
+                free_blocks.remove(i);
+            }
+        }
+    }
+
+    files
+        .into_iter()
+        .map(|f| f.0.sum::<usize>() as u64 * f.1)
+        .sum()
 }
 
 #[cfg(test)]
@@ -51,6 +86,12 @@ mod tests {
     fn test1() {
         let file_content = fs::read_to_string("./sample1.txt").unwrap();
         assert_eq!(part1(file_content), 1928);
+    }
+
+    #[test]
+    fn test2() {
+        let file_content = fs::read_to_string("./sample2.txt").unwrap();
+        assert_eq!(part2(file_content), 2858);
     }
 }
 
@@ -63,6 +104,6 @@ fn main() {
 
     println!(
         "Output: {}",
-        part1(fs::read_to_string(input_path.unwrap()).unwrap())
+        part2(fs::read_to_string(input_path.unwrap()).unwrap())
     );
 }
